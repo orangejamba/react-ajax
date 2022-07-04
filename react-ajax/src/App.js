236ead14927b7d1cb26e113c05e496e33673c8ef
orangejamba <qorwhdqls123@naver.com>
2022-07-04 15:15:14 +0900

    create-react

---
+++ react-ajax/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>web</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="1">HTML</a>
+          </li>
+          <li>
+            <a href="2">CSS</a>
+          </li>
+          <li>
+            <a href="3">JS</a>
+          </li>
+        </ul>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, React &amp; Ajax
+      </article>
+    </div>
+  );
+}
+
+export default App;
