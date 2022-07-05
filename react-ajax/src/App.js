18f684302f3ed30df52266d06cab17dae09da09c
orangejamba <qorwhdqls123@naver.com>
2022-07-05 09:35:21 +0900

    ajax

--- react-ajax/src/App.js
+++ react-ajax/src/App.js
@@ -1,28 +1,90 @@
+import { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>web</h1>
+class Nav extends Component {
+  state = {
+    list: [],
+  };
+  componentDidMount() {
+    fetch("list.json")
+      .then(function (result) {
+        return result.json();
+      })
+      .then(
+        function (json) {
+          console.log(json);
+          this.setState({ list: json });
+        }.bind(this)
+      );
+  }
+  render() {
+    let listTag = [];
+    for (let i = 0; i < this.state.list.length; i++) {
+      let li = this.state.list[i];
+      listTag.push(
+        <li key={li.id}>
+          <a
+            href={li.id}
+            data-id={li.id}
+            onClick={function (e) {
+              e.preventDefault();
+              this.props.onClick(e.target.dataset.id);
+            }.bind(this)}
+          >
+            {li.title}
+          </a>
+        </li>
+      );
+    }
+    return (
       <nav>
-        <ul>
-          <li>
-            <a href="1">HTML</a>
-          </li>
-          <li>
-            <a href="2">CSS</a>
-          </li>
-          <li>
-            <a href="3">JS</a>
-          </li>
-        </ul>
+        <ul>{listTag}</ul>
       </nav>
+    );
+  }
+}
+
+class Article extends Component {
+  render() {
+    return (
       <article>
-        <h2>Welcome</h2>
-        Hello, React &amp; Ajax
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article>
-    </div>
-  );
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    article: { title: "Welcome", desc: "Hello, React & Ajax" },
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>web</h1>
+        <Nav
+          onClick={function (id) {
+            fetch(id + ".json")
+              .then(function (result) {
+                return result.json();
+              })
+              .then(
+                function (json) {
+                  this.setState({
+                    article: { title: json.title, desc: json.desc },
+                  });
+                }.bind(this)
+              );
+          }.bind(this)}
+        ></Nav>
+        <Article
+          title={this.state.article.title}
+          desc={this.state.article.desc}
+        ></Article>
+      </div>
+    );
+  }
 }
 
 export default App;
